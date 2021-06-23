1a5dc83a0541924fac6127fd8d9960c5f0ae69bd
Kristoffer Linder <linderkristoffer@hotmail.com>
2021-06-23 18:21:05 +0200

    Exercise 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,39 +1,56 @@
 import React from 'react';
 
-const Hello = (props) => {
+// const Part = (props) => (
+//     <div>
+//         <p>{props.part} - {props.exercises}</p>
+//     </div>
+// )
+
+// const Content = (props) => {
+//     return (
+//         <div>
+//             <Part part='Fundamentals of React' exercises='10'/>
+//             <Part part='Using props to pass data' exercises='7'/>
+//             <Part part='State of a component' exercises='14'/>
+//         </div>
+//     )
+// }
+const Header = (props) => (
+    <div>
+        <h1>Course: {props.course}</h1>
+    </div>
+)
+
+const Content = (props) => (
+    <div>
+        <p>{props.part} - {props.exercises} exercises</p>
+    </div>
+)
+
+const Total = (props) => (
+    <div>
+        <p>Number of exercises: {props.exercises}</p>
+    </div>
+)
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+  
     return (
         <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
+            <Header course={course} />
+            <Content part={part1} exercises={exercises1} />
+            <Content part={part2} exercises={exercises2} />
+            <Content part={part3} exercises={exercises3} />
+            <Total exercises={exercises1 + exercises2 + exercises3}/>
         </div>
     )
 }
 
-const Footer = () => {
-    return (
-        <div>
-            greeting app created by <a href="https://github.com/liiinder">Kristoffer Linder</a>
-        </div> 
-    )
-}
-
-const App = () => {
-    const now = new Date();
-    const a = 10;
-    const b = 20;
-    console.log('hello from component');
-
-    return (
-        <>
-            <p>Hello world, it is {now.toString()}</p>
-            <p>
-                {a} plus {b} is {a + b}
-            </p>
-            <Hello name="Kristoffer" age={34}/>
-            <Hello name="Joakim" age={32}/>
-            <Hello name="Martin" age={27}/>
-            <Footer />
-        </>
-    )
-};
-
-export default App;
+export default App
