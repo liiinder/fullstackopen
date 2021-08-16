bfe2d377b358aa13bde342ece6307cd6b81156db
Kristoffer Linder <linderkristoffer@hotmail.com>
2021-08-16 20:35:51 +0200

    part2 D done until exercise 2.15

--- part2/notebook/src/components/Note.js
+++ part2/notebook/src/components/Note.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-const Note = ({ note }) => (
-    <li>{note.content}</li>
-)
+const Note = ({ note, toggleImportance }) => {
+    // const label = note.important
+    //     ? 'make not important' : 'make important'
+
+    return (
+        <li>
+            <input onClick={toggleImportance} type="checkbox" id={note.id}
+            checked={note.important} readOnly
+            />
+            {note.content}
+            {/* <button onClick={toggleImportance}>{label}</button> */}
+        </li>
+    )
+}
 
 export default Note
