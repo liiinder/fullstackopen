3fe233ab99d6ff2030f523f0d7c629fca24294de
Kristoffer Linder <linderkristoffer@hotmail.com>
2022-04-13 11:59:37 +0200

    c updated

--- part1/c/src/App.js
+++ part1/c/src/App.js
@@ -1,89 +1,29 @@
-import React, {useState} from "react";
+import { useState } from 'react'
 
-const Hello = ({name, age}) => {
-    // const {name, age} = props
+const Display = ({counter}) => <div>{counter}</div>
 
-    const bornYear = () => new Date().getFullYear() - age
-
+const Button = ({ onClick, text }) => {
     return (
-        <div>
-            <p>Hello {name}, you are {age} years old</p>
-            <p>So you were probably born in {bornYear()}</p>
-        </div>
-    )
-}
-
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({handleClick, text}) => {
-    return (
-        <Button onClick={handleClick}>\
+        <button onClick={onClick}>
             {text}
-        </Button>
+        </button>
     )
 }
 
 const App = () => {
-    const name = 'Peter'
-    const age = 10
     const [ counter, setCounter ] = useState(0)
-    // const handleClick = () => {
-    //     console.log('clicked')
-    // }
-
-    // setTimeout(
-    //     () => setCounter(counter + 1),
-    //     1000
-    // )
     const increaseByOne = () => setCounter(counter + 1)
     const decreaseByOne = () => setCounter(counter - 1)
     const setToZero = () => setCounter(0)
 
-
-    console.log('rendering...', counter);
-    // const {counter} = props
-
     return (
         <div>
             <Display counter={counter} />
-            <h1>Greetings</h1>
-
-            {/* <button onClick={handleClick}>
-                plus
-            </button> */}
-            {/* <button onClick={() => console.log('clicked')}>
-                plus
-            </button> */}
-
-            {/* <button onClick={() => setCounter(counter + 1)}>
-                plus
-            </button>
-            <button onClick={() => setCounter(0)}>
-                zero
-            </button>
-            <button onClick={() => setCounter(counter - 1)}>
-                minus
-            </button> */}
-
-            {/* <button onClick={increaseByOne}>plus</button>
-            <button onClick={setToZero}>zero</button>
-            <button onClick={decreaseByOne}>minus</button> */}
-            <Button
-                handleClick={increaseByOne}
-                text='plus'
-            />
-            <Button
-                handleClick={setToZero}
-                text='zero'
-            />
-            <Button
-                handleClick={decreaseByOne}
-                text='minus'
-            />
-            <Hello name="Maya" age={26 + 10} />
-            <Hello name={name} age={age} />
+            <Button onClick={increaseByOne} text="plus" />
+            <Button onClick={setToZero} text="zero" />
+            <Button onClick={decreaseByOne} text="minus" />
         </div>
     )
 }
 
-export default App;
+export default App
--- part1/c/src/index.js
+++ part1/c/src/index.js
@@ -1,21 +1,7 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />,
+ReactDOM.render(
+    <App />, 
     document.getElementById('root')
 )
-
-// let counter = 1
-
-// const refresh = () => {
-//     ReactDOM.render(
-//         <App counter={counter} />,
-//         document.getElementById('root')
-//     )
-// }
-
-// setInterval(() => {
-//     refresh()
-//     counter += 1
-//   }, 1000)
-
