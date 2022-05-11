7e434bd52188f9154b3dcca13832b0c1f769e642
Kristoffer Linder <linderkristoffer@hotmail.com>
2022-05-11 10:37:28 +0200

    added eslint config to blog

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -16,7 +16,7 @@ mongoose.connect(config.MONGODB_URI)
     .catch((error) => {
         logger.error('error connecting to MongoDB: ', error.message)
     })
-
+/
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
