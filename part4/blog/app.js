5cd35dc0b17c3c93df5c0a1cd318577c9d36d15c
Kristoffer Linder <linderkristoffer@hotmail.com>
2022-05-11 10:19:32 +0200

    4.2 refractored middleware on the blog app

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,7 +3,22 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 require('dotenv').config()
+
+logger.info('connecting to ', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB: ', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -14,27 +29,23 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.get('/api/blogs', (request, response) => {
-    Blog
-    .find({})
-    .then(blogs => {
-        response.json(blogs)
-    })
+    Blog.find({}).then(blogs => { response.json(blogs) })
 })
 
 app.post('/api/blogs', (request, response) => {
     const blog = new Blog(request.body)
-    
+
     blog
-    .save()
-    .then(result => {
-        response.status(201).json(result)
-    })
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
