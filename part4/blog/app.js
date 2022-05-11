16f0074fd1710f05ed0452f3c7e9d4a52ce8d724
Kristoffer Linder <linderkristoffer@hotmail.com>
2022-05-11 10:31:47 +0200

    4.2 Blog list, step 2 done

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,12 +1,11 @@
-const config = require('./utils/config')
-const express = require('express')
 const app = express()
+const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-require('dotenv').config()
 
 logger.info('connecting to ', config.MONGODB_URI)
 
@@ -20,24 +19,8 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-
 app.use(middleware.requestLogger)
-
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then(blogs => { response.json(blogs) })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
+app.use('/api/blogs', blogsRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
